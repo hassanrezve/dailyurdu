beea0e4f5a1922279ea5b123c8f71d907d4d0726
Hassan rezve <rezveshah1211@gmail.com>
2025-07-15 19:55:57 +0500

    sidebar second add added

---
+++ resources/views/partials/sidebar-add2.blade.php
@@ -0,0 +1,14 @@
+<div class="bg-white rounded-2xl shadow-lg p-6 mb-8 border border-slate-100">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1790652247481380"
+            crossorigin="anonymous"></script>
+    <!-- sidebar 2 -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-1790652247481380"
+         data-ad-slot="2430736225"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
