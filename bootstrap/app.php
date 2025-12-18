4c7d255b7c6b2ce4cfc00a420ea471a370ef8472
Hassanrezve <rezveshah1211@gmail.com>
2025-12-18 10:22:20 +0500

    Ignore public/index.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$basePath = dirname(__DIR__);
+
+$publicPath = $basePath . '/public';
+
+$sharedPublic = dirname($basePath) . '/public_html';
+
+$app = Application::configure(basePath: $basePath)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -15,4 +21,12 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
+
+if (is_dir($sharedPublic)) {
+    $publicPath = $sharedPublic;
+}
+$app->usePublicPath($publicPath);
+
+return $app;
